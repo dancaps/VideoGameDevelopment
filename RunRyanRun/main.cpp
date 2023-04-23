7444083e09368950d4ac26aea31ae47065734d39
Daniel Caperton <danny.caperton@gmail.com>
2023-04-23 16:14:45 -0600

    UPDATE: big changes as I learned more

--- RunRyanRun/main.cpp
+++ RunRyanRun/main.cpp
@@ -1,5 +1,14 @@
 #include "raylib.h"
 
+struct AnimationData
+{
+    Rectangle rec;
+    Vector2 pos;
+    int frame;
+    float updateTime;
+    float runningTime;
+};
+
 int main()
 {
     // game setup variables
@@ -10,27 +19,39 @@ int main()
     // window configuration
     InitWindow(gameWidth, gameHeight, gameTitle);
 
+    // nebula setup
+    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+    AnimationData nebulaAnimation = {
+        {0.0, 0.0, static_cast<float>(nebula.width / 8.0), static_cast<float>(nebula.height / 8.0)}, // Rectangle Data
+        {gameWidth, gameHeight - static_cast<float>(nebula.height / 8.0)}, // Vector2 data
+        0, // frame initialization
+        1.0 / 12.0, // updateTime
+        0.0, // runningTime
+    };
+    AnimationData nebula2Animation = {
+        {0.0, 0.0, static_cast<float>(nebula.width / 8.0), static_cast<float>(nebula.height / 8.0)}, // Rectangle Data
+        {gameWidth + gameWidth / 2, gameHeight - static_cast<float>(nebula.height / 8.0)}, // Vector2 data
+        0, // frame initialization
+        1.0 / 18.0, // updateTime
+        0.0, // runningTime
+    };
+
     // ryan setup
     Texture2D ryan = LoadTexture("textures/ryan.png");
-    Rectangle ryanRec;
-    ryanRec.width = ryan.width / 6;
-    ryanRec.height = ryan.height;
-    ryanRec.x = 0;
-    ryanRec.y = 0;
-    Vector2 ryanPos;
-    ryanPos.x = gameWidth / 3;
-    ryanPos.y = gameHeight - ryanRec.height;
-
-    // animation frame
-    int frame{};
-    const float updateTime{1.0 / 12.0};
-    float runningTime{};
+    AnimationData ryanAnimation = {
+        {0.0, 0.0, static_cast<float>(ryan.width / 6.0), static_cast<float>(ryan.height)},
+        {gameWidth / 3, gameHeight - static_cast<float>(ryan.height)}, 
+        0,
+        1.0 / 12.0,
+        0.0
+    };
 
     // game physics
     const int jumpVelocity{-1500}; // pixels/sec
     int velocity{0}; 
     bool isInAir{};
     const int gravity{4000}; // (pixels/sec)/sec
+    const int nebulaVelocity{-500};
 
     SetTargetFPS(120);
 
@@ -43,53 +64,102 @@ int main()
         // gets the delta time
         float dT{GetFrameTime()};
 
-        runningTime += GetFrameTime();
+        // stores the running time since the last reset
+        ryanAnimation.runningTime += GetFrameTime();
+        nebulaAnimation.runningTime += GetFrameTime();
+        nebula2Animation.runningTime += GetFrameTime();
 
         // ground check
-        if (ryanPos.y < gameHeight - ryanRec.height)
+        if (ryanAnimation.pos.y < gameHeight - ryanAnimation.rec.height)
         {
-            // we are in the air
+            // player in the air
             velocity += gravity * dT; // increases gravity using delta time
             isInAir = true; // updates the flag to show in the air
         }
         else
         {
-            // we are on the ground
+            // player on the ground
             velocity = 0; // stops falling
-            ryanPos.y = gameHeight - ryanRec.height; // resets the postion to the ground
+            ryanAnimation.pos.y = gameHeight - ryanAnimation.rec.height; // resets the postion to the ground
             isInAir = false; // updates flag showing not in the air
         }
 
         // jump when not already in the air
         if (IsKeyPressed(KEY_SPACE) && !isInAir)
         {
-            velocity += jumpVelocity; // Applies the jumpVelocity to velocity to make the jump
+            velocity += jumpVelocity; // applies the jumpVelocity to velocity to make the jump
         }
 
-        ryanPos.y += velocity * dT;
+        // animation postion
+        ryanAnimation.pos.y += velocity * dT;
+        nebulaAnimation.pos.x += nebulaVelocity * dT;
+        nebula2Animation.pos.x += nebulaVelocity * dT;
 
-        // Update animation frame
-        ryanRec.x = frame * ryanRec.width;
+        // update animation frame
+        nebulaAnimation.rec.x = nebulaAnimation.frame * nebulaAnimation.rec.width;
+        nebula2Animation.rec.x = nebula2Animation.frame * nebula2Animation.rec.width;
+        //nebulaRec.x = nebulaFrame * nebulaRec.width;
 
-        if (runningTime >= updateTime) 
+        // ryan moving
+        if (ryanAnimation.runningTime >= ryanAnimation.updateTime) 
         {
-            //time to update the frame
-            frame++;
-            if (frame > 5)
+            // update the player frame
+            if (!isInAir)
             {
-                frame = 0;
+                ryanAnimation.rec.x = ryanAnimation.frame * ryanAnimation.rec.width;
+                ryanAnimation.frame++;
+                if (ryanAnimation.frame > 5)
+                {
+                    ryanAnimation.frame = 0;
+                }
             }
-            runningTime = 0.0;
+            else
+            {
+                // uses the first image in the sprite as the jump image
+                ryanAnimation.rec.x = 0.0;
+            }
+            ryanAnimation.runningTime = 0.0;
         }
 
-        // draw rectangle
-        DrawTextureRec(ryan, ryanRec, ryanPos, WHITE);
+        // first nebula
+        if (nebulaAnimation.runningTime >= nebulaAnimation.updateTime)
+        {
+            // time to update the nebula frame
+            nebulaAnimation.frame++;
+
+            if (nebulaAnimation.frame > 7)
+            {
+                nebulaAnimation.frame = 0;
+            }
+
+            nebulaAnimation.runningTime = 0.0;
+        }
+
+        // second nebula
+        if (nebula2Animation.runningTime >= nebula2Animation.updateTime)
+        {
+            // time to update the nebula frame
+            nebula2Animation.frame++;
+
+            if (nebula2Animation.frame > 7)
+            {
+                nebula2Animation.frame = 0;
+            }
+
+            nebula2Animation.runningTime = 0.0;
+        }
+
+        // draw animations
+        DrawTextureRec(ryan, ryanAnimation.rec, ryanAnimation.pos, WHITE);
+        DrawTextureRec(nebula, nebulaAnimation.rec, nebulaAnimation.pos, WHITE);
+        DrawTextureRec(nebula, nebula2Animation.rec, nebula2Animation.pos, RED);
 
         // end drawing
         EndDrawing();
     }
 
     UnloadTexture(ryan);
+    UnloadTexture(nebula);
     CloseWindow();
 
     // return
