e8fff221506d0703daf92edaeb928302646587f7
Daniel Caperton <danny.caperton@gmail.com>
2023-04-25 20:51:56 -0600

    UPDATE: Added background images
    Still has bug in the background image scrolling off the screen

--- RunRyanRun/main.cpp
+++ RunRyanRun/main.cpp
@@ -9,6 +9,18 @@ struct AnimationData
     float runningTime;
 };
 
+AnimationData updateAnimationData(AnimationData data, float deltaTime, int maxFrame)
+{
+    data.rec.x = data.frame * data.rec.width;
+    data.frame++;
+    if (data.frame > maxFrame)
+    {
+        data.frame = 0;
+    }
+    data.runningTime = 0.0;
+    return data;
+}
+
 int main()
 {
     // game setup variables
@@ -52,18 +64,56 @@ int main()
     const int gravity{4000}; // (pixels/sec)/sec
     const int nebulaVelocity{-500};
 
+    // background setup
+    Texture2D background = LoadTexture("textures/far-buildings.png");
+    float bgX{};
+    Texture2D midground = LoadTexture("textures/back-buildings.png");
+    float mgX{};
+    Texture2D foreground = LoadTexture("textures/foreground.png");
+    float fgX{};
+    const float backgroundScale{4.5};
+
     // sets the fps
     SetTargetFPS(120);
 
     // running the game
     while (!WindowShouldClose()) 
     {
+        // gets the delta time
+        float dT{GetFrameTime()};
+
         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
 
-        // gets the delta time
-        float dT{GetFrameTime()};
+        // drawing background
+        bgX -= 20 * dT;
+        if (bgX <= -background.width * backgroundScale)
+        {
+            bgX = 0.0;
+        }
+        Vector2 bg1Pos{bgX, 0.0};
+        DrawTextureEx(background, bg1Pos, 0.0, 4.5, WHITE);
+        Vector2 bg2Pos{static_cast<float>(bgX + background.width * 4.5), 0.0};
+        DrawTextureEx(background, bg2Pos, 0.0, 4.5, WHITE);
+        mgX -= 40 * dT;
+        if (mgX <= -midground.width * backgroundScale)
+        {
+            mgX = 0.0;
+        }
+        Vector2 mg1Pos{mgX, 0.0};
+        DrawTextureEx(midground, mg1Pos, 0.0, 4.5, WHITE);
+        Vector2 mg2Pos{static_cast<float>(mgX + midground.width * 4.5), 0.0};
+        DrawTextureEx(midground, mg2Pos, 0.0, 4.5, WHITE);
+        fgX -= 80 * dT;
+        if (fgX <= -foreground.width * backgroundScale)
+        {
+            fgX = 0.0;
+        }
+        Vector2 fg1Pos{fgX, 0.0};
+        DrawTextureEx(foreground, fg1Pos, 0.0, 4.5, WHITE);
+        Vector2 fg2Pos{static_cast<float>(fgX + foreground.width * 4.5), 0.0};
+        DrawTextureEx(foreground, fg2Pos, 0.0, 4.5, WHITE);
 
         // stores the running time since the last reset
         ryanAnimation.runningTime += GetFrameTime();
@@ -116,19 +166,14 @@ int main()
             // update the player frame
             if (!isInAir)
             {
-                ryanAnimation.rec.x = ryanAnimation.frame * ryanAnimation.rec.width;
-                ryanAnimation.frame++;
-                if (ryanAnimation.frame > 5)
-                {
-                    ryanAnimation.frame = 0;
-                }
+                ryanAnimation = updateAnimationData(ryanAnimation, dT ,5);
             }
             else
             {
                 // uses the first image in the sprite as the jump image
                 ryanAnimation.rec.x = 0.0;
             }
-            ryanAnimation.runningTime = 0.0;
+            
         }
 
         // nebulae moving
@@ -138,14 +183,7 @@ int main()
             if (nebulae[i].runningTime >= nebulae[i].updateTime)
             {
                 // time to update the nebula frame
-                nebulae[i].frame++;
-
-                if (nebulae[i].frame > 7)
-                {
-                    nebulae[i].frame = 0;
-                }
-
-                nebulae[i].runningTime = 0.0;
+                nebulae[i] = updateAnimationData(nebulae[i], dT ,7);
             }
         }
 
@@ -163,6 +201,9 @@ int main()
     }
 
     // cleanup
+    UnloadTexture(background);
+    UnloadTexture(midground);
+    UnloadTexture(foreground);
     UnloadTexture(ryan);
     UnloadTexture(nebula);
     CloseWindow();
